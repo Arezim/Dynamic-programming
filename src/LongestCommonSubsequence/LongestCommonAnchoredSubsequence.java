72fa5413ccd7f890e68c91168054a3f22fea2c34
Tomasz Mizera <t.mizera@student.maastrichtuniversity.nl>
2024-04-25 11:24:19 +0200

    LongestCommonAnchoredSubsequence

--- src/LongestCommonSubsequence/LongestCommonAnchoredSubsequence.java
+++ src/LongestCommonSubsequence/LongestCommonAnchoredSubsequence.java
@@ -1,7 +1,7 @@
 package LongestCommonSubsequence;
 public class LongestCommonAnchoredSubsequence {
     public static void main(String[] args){
-        System.out.println("wynik: " + getLCS("ab*aaa", "ab***aaa")); //does not work for "****" and "a*b*cd*"
+        System.out.println("solution: " + getLCS("****", "a*b*cd*"));
     }
 
     private static int getLCS(String string1, String string2){
@@ -24,24 +24,17 @@ public class LongestCommonAnchoredSubsequence {
                 if(i == 0 || j == 0){
                     arr[i][j] = 0;
                 }
-                // rozne litery, rozne od gwiazdek, patrzą, czy nie ma gwiazdek na skos
-                else if(i > 1 && j > 1 && (string1.charAt(i-2) == '*' && string2.charAt(j-2) == '*') && (string1.charAt(i-1) != string2.charAt(j-1)) && (string1.charAt(i-1) != '*') && (string2.charAt(j-1) != '*')){
-                    arr[i][j] = arr[i-1][j-1];
-                }
-                // takie same litery, rozne od gwiazdek, patrzą na skos, i boki
-                else if((string1.charAt(i-1) == string2.charAt(j-1)) && string1.charAt(i-1) != '*') {
-                    arr[i][j] = Math.max(arr[i - 1][j - 1] + 1, Math.max(arr[i - 1][j], arr[i][j - 1]));
-                }
-                // * i * patrzą tylko na skos
-                else if((string1.charAt(i-1) == '*' && string2.charAt(j-1) == '*')){
+                else if((string1.charAt(i-1) == string2.charAt(j-1))){
                     arr[i][j] = arr[i-1][j-1] + 1;
                 }
-                // * i coś innego sie zeruje
-                else if((string1.charAt(i-1) == '*') || (string2.charAt(j-1) == '*')){
-                    arr[i][j] = 0;
+                else if(string2.charAt(j-1) == '*'){
+                    arr[i][j] = arr[i-1][j];
+                }
+                else if(string1.charAt(i-1) == '*'){
+                    arr[i][j] = arr[i][j-1];
                 }
                 else{
-                    arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
+                    arr[i][j] = Math.max(arr[i][j-1], arr[i-1][j]);
                 }
                 if(arr[i][j] > max){
                     max = arr[i][j];
